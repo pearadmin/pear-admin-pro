83c35e5fea0906e2a87d9b05a6f7340ef5abd89c
就眠仪式 <854085467@qq.com>
2021-04-03 12:07:37 +0800

    commit code

--- backend/src/main/java/com/pearadmin/pro/modules/sys/domain/request/QueryUserRequest.java
+++ backend/src/main/java/com/pearadmin/pro/modules/sys/domain/request/QueryUserRequest.java
@@ -1,17 +1,19 @@
 package com.pearadmin.pro.modules.sys.domain.request;
 
 import lombok.Data;
+import org.apache.ibatis.type.Alias;
 
 /**
  * Describe: 用户查询参数实体
  * Author: 就 眠 仪 式
- * CreateTime: 2019/10/23
+ * CreateTime: 2021/04/24
  * */
 @Data
+@Alias("QueryUserRequest")
 public class QueryUserRequest {
 
     /**
-     * 用户名
+     * 账户
      * */
     private String username;
 
