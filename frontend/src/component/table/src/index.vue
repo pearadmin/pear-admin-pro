25d3c61696f3e5e7c003bdf22973341a44efca62
就眠儀式 <854085467@qq.com>
2021-11-16 23:42:46 +0800

    😏 修复 pro-table 组件自定义 slot 失效

--- frontend/src/component/table/src/index.vue
+++ frontend/src/component/table/src/index.vue
@@ -197,13 +197,18 @@
         </span>
 
       </template>
+
+            <template v-for="name in slotsData" #[name]="{ record }">
+        <slot :name="name" :record="record"></slot>
+      </template>
+
     </a-table>
   </div>
 </template>
 <script>
 import "./index.less";
 import T from "ant-design-vue/es/table/Table";
-import { defineComponent, onMounted, reactive, toRefs, watch } from "vue";
+import { defineComponent, onMounted, reactive, toRefs, watch, ref } from "vue";
 import {
   AppstoreOutlined,
   ExportOutlined,
@@ -257,6 +262,15 @@ export default defineComponent({
     },
   }),
   setup(props) {
+
+    const slotsData = ref([]);
+
+    props.columns.map((value) => {
+      if (value.slots) {
+        slotsData.value.push(value.slots.customRender);
+      }
+    });
+
     /// 状态共享
     const state = reactive({
       pagination: props.pagination == false ? false : props.pagination, // 分页
@@ -372,6 +386,8 @@ export default defineComponent({
       changeSize,
       /// 打印
       print,
+
+      slotsData
     };
   },
 });
